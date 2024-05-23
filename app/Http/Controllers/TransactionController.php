60af8c42dedae66bffeb8fc406fd9fc8e111c9d3
Usman Lubis <usmanlubis11@gmail.com>
2024-05-23 18:54:04 +0700

    add transaction controler

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index() {
+        $transactions = [
+            [
+                "id" => 1,
+                "userId" => 4,
+                "carName" => "Honda BRV",
+                "carImage" => "https://i.postimg.cc/Xqm60y93/daihatsu-xenia.png",
+                "returnDate" => "31-05-2024",
+                "totalPrice" => 700000,
+                "isActive" => true,
+            ],
+            [
+                "id" => 2,
+                "userId" => 4,
+                "carName" => "BMW F30",
+                "carImage" => "https://i.postimg.cc/q7kYSGWy/bmw-f30.png",
+                "returnDate" => "31-05-2024",
+                "totalPrice" => 4500000,
+                "isActive" => true,
+            ]
+        ];
+
+        return view('transaction', [
+            "transactions" => $transactions
+        ]);
+    }
+}
