4987003d136525267ff473c2b87e2af8e2626cb6
Usman Lubis <usmanlubis11@gmail.com>
2024-05-23 10:07:27 +0700

    add detail page

--- resources/views/car/detail.blade.php
+++ resources/views/car/detail.blade.php
@@ -1,1 +1,37 @@
-<h1>{{$car['name']}}</h1>
+@extends("layouts.app")
+
+@section("title")
+  {{ $car['name'] }}
+@endsection
+
+@section("content")
+  <div class="w-full max-w-[1440px] p-4">
+    <h1 class="mb-8 text-3xl font-bold text-center">{{ $car['name'] }} Details</h1>
+    <div class="mx-auto w-full max-w-[780px] flex justify-center">
+      <div class="w-[50%]">
+        <img src="{{ $car['image'] }}" alt="{{ $car['name'] }}">
+      </div>
+      <div class="w-[50%] flex flex-col gap-4">
+        <p class="font-bold">{{ $car['name'] }} ({{ $car['year'] }})</p>
+        <p>Rp. {{ number_format($car['price'], 0, ',', '.') }}/day</p>
+        <p class="text-justify">{{ $car['description'] }}</p>
+        <form action="" class="flex flex-col gap-2">
+          <div class="flex flex-col gap-1">
+            <label for="start">Start Date</label>
+            <input type="date" name="start" id="start" class="input w-full p-1 rounded-md focus:outline-2 focus:outline-erentGreen" required>
+          </div>
+          <div class="flex flex-col gap-1">
+            <label for="return">Return Date</label>
+            <input type="date" name="return" id="return" class="input w-full p-1 rounded-md focus:outline-2 focus:outline-erentGreen" required>
+          </div>
+          <div>
+            <p class="mt-4 font-bold text-xl">Total price: </p>
+          </div>
+          <div>
+            <button type="submit" class="w-full mt-4 bg-erentGreen active:bg-erentGreen/15 transition duration-100 ease-in-out text-white px-4 py-2 rounded-md hover:bg-white hover:text-erentGreen hover:outline hover:outline-erentGreen">Rent</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+@endsection
