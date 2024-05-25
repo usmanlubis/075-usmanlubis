395441a77b70d82b2f05fe7d217b94c78afe77c8
Usman Lubis <usmanlubis11@gmail.com>
2024-05-25 21:34:07 +0700

    add responsive contact page and footer

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,8 +8,8 @@
     <x-hero>
         <div class="relative h-full w-full flex justify-center items-center p-4">
             <div class="w-full max-w-[1080px] bg-erentYellow rounded-xl shadow-lg p-6">
-                <form action="" class="flex gap-4 justify-between">
-                    <div class="flex flex-col gap-4 w-[50%] max-w-[450px]">
+                <form action="" class="flex flex-col md:flex-row gap-4 justify-between">
+                    <div class="flex flex-col gap-4 w-full md:w-[50%] max-w-[450px]">
                         <div class="flex flex-col">
                             <label for="name">Name</label>
                             <input type="text" name="email" id="name" required class="w-full p-1 rounded-md focus:outline-2 focus:outline-erentGreen">
@@ -19,7 +19,7 @@
                             <input type="email" name="email" id="email" required class="w-full p-1 rounded-md focus:outline-2 focus:outline-erentGreen">
                         </div>
                     </div>
-                    <div class="flex flex-col gap-4 w-[50%] max-w-[450px]">
+                    <div class="flex flex-col gap-4 w-full md:w-[50%] max-w-[450px]">
                         <div class="flex flex-col">
                             <label for="message">Message</label>
                             <textarea name="message" id="message" rows="3" class="w-full p-1 rounded-md focus:outline-2 focus:outline-erentGreen"></textarea>
