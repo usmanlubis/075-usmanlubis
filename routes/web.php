ced81cbd2111b78dd45ce4d4e03404a2659c4ec5
Usman Lubis <usmanlubis11@gmail.com>
2024-05-27 17:17:21 +0700

    refactor: make code consistent

--- routes/web.php
+++ routes/web.php
@@ -9,33 +9,33 @@ use App\Http\Middleware\UserIsLoggedIn;
 use App\Http\Middleware\UserRole;
 
 Route::controller(CarController::class)->middleware(UserIsLoggedIn::class)->group(function() {
-    Route::get('/', "index")->name("homepage");
-    Route::get('/cars', "show")->name("cars");
-    Route::get('/car/{id}', "detail")->name("car-detail");
-    Route::get('/add', "create")->name("add")->middleware(UserRole::class);
-    Route::post('/add', "store")->name("car-store")->middleware(UserRole::class);
-    Route::get('/edit/{id}', "edit")->name("car.edit")->middleware(UserRole::class);
-    Route::patch('/edit/{id}', "update")->name("car-update")->middleware(UserRole::class);
-    Route::get('/delete/{id}', "delete")->name("car.delete")->middleware(UserRole::class);
-    Route::delete('/delete/{id}', "destroy")->name("car-destroy")->middleware(UserRole::class);
+    Route::get("/", "index")->name("homepage");
+    Route::get("/car/{id}", "detail")->name("car-detail");
+    Route::get("/add", "create")->name("add")->middleware(UserRole::class);
+    Route::post("/add", "store")->name("car-store")->middleware(UserRole::class);
+    Route::get("/edit/{id}", "edit")->name("car.edit")->middleware(UserRole::class);
+    Route::get("/cars", "show")->name("cars");
+    Route::patch("/edit/{id}", "update")->name("car-update")->middleware(UserRole::class);
+    Route::get("/delete/{id}", "delete")->name("car.delete")->middleware(UserRole::class);
+    Route::delete("/delete/{id}", "destroy")->name("car-destroy")->middleware(UserRole::class);
 });
 
 Route::controller(TransactionController::class)->middleware(UserIsLoggedIn::class)->group(function() {
-    Route::get('/transaction', "index")->name("transaction");
-    Route::post('/car/{id}', "store")->name("transaction-store");
-    Route::patch('/transaction', "update")->name("transaction-update")->middleware(UserRole::class);
+    Route::get("/transaction", "index")->name("transaction");
+    Route::post("/car/{id}", "store")->name("transaction-store");
+    Route::patch("/transaction", "update")->name("transaction-update")->middleware(UserRole::class);
 });
 
 Route::controller(AuthController::class)->group(function() {
-    Route::get('/login', "loginPage")->name('login-page');
-    Route::post('/login', "login")->name('login');
-    Route::get('/signup', "signupPage")->name('signup-page');
-    Route::post('/signup', "signup")->name('signup');
-    Route::get('/logout', "logout")->name('logout')->middleware(UserIsLoggedIn::class);
+    Route::get("/login", "loginPage")->name("login-page");
+    Route::post("/login", "login")->name("login");
+    Route::get("/signup", "signupPage")->name("signup-page");
+    Route::post("/signup", "signup")->name("signup");
+    Route::get("/logout", "logout")->name("logout")->middleware(UserIsLoggedIn::class);
 });
 
-Route::view('/about', 'about')->name('about')->middleware(UserIsLoggedIn::class);
+Route::view("/about", "about")->name("about")->middleware(UserIsLoggedIn::class);
 
-Route::view('/contact', 'contact')->name('contact')->middleware(UserIsLoggedIn::class);
+Route::view("/contact", "contact")->name("contact")->middleware(UserIsLoggedIn::class);
 
-Route::fallback(fn() => response()->view('404'))->name('404');
+Route::fallback(fn() => response()->view("404"))->name("404");
