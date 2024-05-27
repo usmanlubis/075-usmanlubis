8533403cef9cbdd91e954c11d395562d3ca2f906
Usman Lubis <usmanlubis11@gmail.com>
2024-05-27 21:44:34 +0700

    style: customize transaction title on admin view

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -1,7 +1,11 @@
 @extends("layouts.app")
 
 @section("title")
+  @if(session()->get('role_id') == 1)
+    Transactions - eRent
+  @else
     My Transactions - eRent
+  @endif
 @endsection
 
 @section("content")
