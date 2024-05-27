3d93ef994f9ce422fad70749c3b5ca6d4d55d332
Usman Lubis <usmanlubis11@gmail.com>
2024-05-27 17:03:06 +0700

    make contact responsive

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,10 +6,10 @@
 
 @section("content")
     <x-hero>
-        <div class="relative h-full w-full flex justify-center items-center p-4">
+        <div class="relative flex items-center justify-center w-full h-full p-4">
             <div class="w-full max-w-[1080px] bg-erentYellow rounded-xl shadow-lg p-6">
-                <form action="" class="flex flex-col md:flex-row gap-4 justify-between">
-                    <div class="flex flex-col gap-4 w-full md:w-[50%] max-w-[450px]">
+                <form action="" class="flex flex-col justify-between gap-4 md:flex-row">
+                    <div class="flex flex-col gap-4 w-full md:w-[50%]">
                         <div class="flex flex-col">
                             <label for="name">Name</label>
                             <input type="text" name="email" id="name" required class="w-full p-1 rounded-md focus:outline-2 focus:outline-erentGreen">
@@ -19,13 +19,13 @@
                             <input type="email" name="email" id="email" required class="w-full p-1 rounded-md focus:outline-2 focus:outline-erentGreen">
                         </div>
                     </div>
-                    <div class="flex flex-col gap-4 w-full md:w-[50%] max-w-[450px]">
+                    <div class="flex flex-col gap-4 w-full md:w-[50%]">
                         <div class="flex flex-col">
                             <label for="message">Message</label>
                             <textarea name="message" id="message" rows="3" class="w-full p-1 rounded-md focus:outline-2 focus:outline-erentGreen"></textarea>
                         </div>
                         <div>
-                            <button class="w-full bg-erentGreen py-2 px-4 rounded-md text-white active:bg-erentGreen/15 transition duration-100 ease-in-out hover:bg-white hover:text-erentGreen hover:outline hover:outline-erentGreen">Submit</button>
+                            <button class="w-full px-4 py-2 text-white transition duration-100 ease-in-out rounded-md bg-erentGreen active:bg-erentGreen/15 hover:bg-white hover:text-erentGreen hover:outline hover:outline-erentGreen">Submit</button>
                         </div>
                     </div>
                 </form>
