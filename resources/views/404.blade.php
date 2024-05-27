6c5728ef7813f778ad80cf41fdbc3d919cc376b5
Usman Lubis <usmanlubis11@gmail.com>
2024-05-27 14:10:57 +0700

    change home link on 404 page with route link

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -12,7 +12,7 @@
         </svg>
         <h1 class="text-4xl font-bold mb-4">404 - Page Not Found</h1>
         <p class="text-lg mb-8">Sorry, the page you are looking for does not exist.</p>
-        <a href="/" class="hover:opacity-90 hover:underline active:opacity-85">Go back home</a>
+        <a href="{{ route('homepage') }}" class="hover:opacity-90 hover:underline active:opacity-85">Go back home</a>
     </div>
   </section>
 @endsection
