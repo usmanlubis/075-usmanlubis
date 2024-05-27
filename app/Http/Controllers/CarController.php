dbc8df15b02f5b2434c02477388af5133c33793e
Usman Lubis <usmanlubis11@gmail.com>
2024-05-27 19:31:00 +0700

    style: write consistent and clean code

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,127 +2,139 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Car;
 use App\Models\Transaction;
+use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $cars = Car::all();
-
         $selectedCars = [];
+
         foreach ($cars as $car) {
-            if ($car['isAvailable']){
+            if ($car["isAvailable"]){
                 $selectedCars[] = $car;
             }
         }
 
-        return view('homepage', [
+        return view("homepage", [
         "cars" => $selectedCars
         ]);
     }
 
-    public function show() {
+    public function show()
+    {
         $cars = Car::all();
 
-        return view('rent', [
+        return view("rent", [
         "cars" => $cars
         ]);
     }
 
-    public function detail(int $id) {
+    public function detail(int $id)
+    {
         $cars = Car::all();
         $selectedCar = null;
+
         foreach ($cars as $car) {
-            if ($car['id'] == $id)
+            if ($car["id"] == $id)
             $selectedCar = $car;
         }
 
-        return view('car.detail', [
+        return view("car.detail", [
             "car" => $selectedCar
         ]);
     }
 
-    public function edit(int $id) {
+    public function edit(int $id)
+    {
         $cars = Car::all();
         $selectedCar = null;
+
         foreach ($cars as $car) {
-            if ($car['id'] == $id)
+            if ($car["id"] == $id)
             $selectedCar = $car;
         }
 
-        return view('car.edit', [
+        return view("car.edit", [
             "car" => $selectedCar
         ]);
     }
 
-    public function delete(int $id) {
+    public function delete(int $id)
+    {
         $cars = Car::all();
         $selectedCar = null;
+
         foreach ($cars as $car) {
-            if ($car['id'] == $id)
+            if ($car["id"] == $id)
             $selectedCar = $car;
         }
 
-        return view('car.delete', [
+        return view("car.delete", [
             "car" => $selectedCar
         ]);
     }
 
-    public function create() {
-        return view('add');
+    public function create()
+    {
+        return view("add");
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $payload = $request->all();
+
         $newCar = [
-            "name" => $payload['name'],
-            "image" => $payload['image'],
-            "year" => $payload['year'],
-            "description" => $payload['description'],
+            "name" => $payload["name"],
+            "image" => $payload["image"],
+            "year" => $payload["year"],
+            "description" => $payload["description"],
             "isAvailable" => true,
-            "price" => $payload['price']
+            "price" => $payload["price"]
         ];
 
         $car = Car::create($newCar);
 
-        return redirect()->route('car-detail', [
-            "id" => $car['id']
+        return redirect()->route("car-detail", [
+            "id" => $car["id"]
         ]);
     }
-    public function update(Request $request) {
+    public function update(Request $request)
+    {
         $payload = $request->all();
-        $id = $payload['id'];
+        $id = $payload["id"];
 
         $updatedCar = [
-            "name" => $payload['name'],
-            "image" => $payload['image'],
-            "year" => $payload['year'],
-            "description" => $payload['description'],
-            "price" => $payload['price']
+            "name" => $payload["name"],
+            "image" => $payload["image"],
+            "year" => $payload["year"],
+            "description" => $payload["description"],
+            "price" => $payload["price"]
         ];
 
         $car = Car::findOrFail($id);
         $car->update($updatedCar);
-
-        $transactions = Transaction::where('car_id', $id)->get();
+        $transactions = Transaction::where("car_id", $id)->get();
 
         foreach ($transactions as $transaction) {
-            $transaction->carName = $payload['name'];
-            $transaction->carImage = $payload['image'];
+            $transaction->carName = $payload["name"];
+            $transaction->carImage = $payload["image"];
             $transaction->save();
         }
 
-        return redirect()->route('car-detail', [
-            'id' => $car['id']
+        return redirect()->route("car-detail", [
+            "id" => $car["id"]
         ]);
     }
 
-    public function destroy(int $id) {
+    public function destroy(int $id)
+    {
         $car = Car::findOrFail($id);
         $car->delete();
 
-        return redirect()->route('cars');
+        return redirect()->route("cars");
     }
 }
